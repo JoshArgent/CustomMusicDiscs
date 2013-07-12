3adf100b797fb82ebbd5cbcd747bc58cf3d2b619
Josh Argent <josh@vanillaworld.org>
2013-07-12 07:55:58 +0100

    Added plugin.yml and onCommand methods.

--- CustomMusicDiscs/src/org/vanillaworld/CustomMusicDiscs/Main.java
+++ CustomMusicDiscs/src/org/vanillaworld/CustomMusicDiscs/Main.java
@@ -1,5 +1,7 @@
 package org.vanillaworld.CustomMusicDiscs;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -15,6 +17,16 @@ public class Main extends JavaPlugin implements Listener {
 	public void onDisable()
 	{
 		plugin = null;
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+	{
+		if(cmd.getName().equalsIgnoreCase("music"))
+		{
+			
+			return true;
+		}
+		return false; 
 	}
 
 }
