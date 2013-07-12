e0e0d0643b54e04d3d9dfbffa28f950cb2bdf832
Josh Argent <josh@vanillaworld.org>
2013-07-12 07:53:29 +0100

    Added default JavaPlugin methods.

--- CustomMusicDiscs/src/org/vanillaworld/CustomMusicDiscs/Main.java
+++ CustomMusicDiscs/src/org/vanillaworld/CustomMusicDiscs/Main.java
@@ -1,5 +1,20 @@
 package org.vanillaworld.CustomMusicDiscs;
 
-public class Main {
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+	
+	public static JavaPlugin plugin;
+	
+	public void onEnable()
+	{
+		plugin = this;
+	}
+	
+	public void onDisable()
+	{
+		plugin = null;
+	}
 
 }
